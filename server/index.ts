603a00f0c0fa957f474e7b5ffac458ca885e0ee0
sezginbozdemir <88796191+sezginbozdemir@users.noreply.github.com>
2024-10-14 15:55:17 +0300

    build

--- server/index.ts
+++ server/index.ts
@@ -23,10 +23,10 @@ mongoose
     app.use("/api", productRoutes);
     app.use("/api", customerRoutes);
     app.use("/api", employeeRoutes);
-    app.use(express.static(path.join(__dirname, "..client/dist")));
+    app.use(express.static(path.join(__dirname, "../client/dist")));
 
     app.get("*", (req: Request, res: Response) => {
-      res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
+      res.sendFile(path.join(__dirname, "../client/dist/index.html"));
     });
 
     app.listen(port, () => console.log(`server running on ${port}`));
